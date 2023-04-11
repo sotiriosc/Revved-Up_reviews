b0e020884277b11c9ba11c95cf8adb969b6c8319
sotiriosc <chortogiannoss@gmail.com>
2023-04-11 08:52:39 -0400

    minor corrections

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,19 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  theme: {
+    screens: {
+      'sm': '400px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+    }
+  }
 }
+
 

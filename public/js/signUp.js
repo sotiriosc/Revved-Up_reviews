864fff99bd9dbefd59e8b87ac57c95c1246d8906
sotiriosc <chortogiannoss@gmail.com>
2023-04-12 23:50:33 -0400

    added password confirm

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -3,23 +3,28 @@ const signupFormHandler = async (event) => {
 
   const username = document.querySelector('#username-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+  const confirmPassword = document.querySelector('#confirm-password-signup').value.trim();
 
-  if (username && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+  if (username && password && confirmPassword) {
+    if (password === confirmPassword) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-      alert("Account Successfully Created")
+      if (response.ok) {
+        document.location.replace('/dashboard');
+        alert("Account Successfully Created");
+      } else {
+        alert(response.statusText);
+      }
     } else {
-      alert(response.statusText);
+      alert("Passwords do not match");
     }
   }
 };
 
 document
-.querySelector('.signup-form')
-.addEventListener('submit', signupFormHandler);
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
